66b56b71b16b48f4545a0a89f4743111de34adc4
HashimotoMakoto <tokoma0310@icloud.com>
2024-03-23 17:51:06 +0900

    プレイヤー(unityちゃん)の設置　プレイヤーの移動作成(途中)

--- UnityTeam/Assets/Script/PlayerMove.cs
+++ UnityTeam/Assets/Script/PlayerMove.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    public float walkSpeed = 1.0f;
+    public float dashSpeed = 2.0f;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,26 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 pos = transform.position;
+        Quaternion rot = transform.rotation;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            pos.x = walkSpeed * Mathf.Cos(rot.x);
+            pos.z = walkSpeed * Mathf.Sin(rot.z);
+            pos.y = 0.0f;
+        }
+        else
+        {
+            pos.x = 0.0f;
+            pos.z = 0.0f;
+            pos.y = 0.0f;
+        }
+
+        Rigidbody rb = GetComponent<Rigidbody>();
+        Vector3 force = new Vector3(pos.x, pos.y, pos.z);
+        rb.AddForce(force, ForceMode.Force);
+//        rb.velocity = pos;
+//        GetComponent<Rigidbody>().AddForce(pos);
     }
 }
